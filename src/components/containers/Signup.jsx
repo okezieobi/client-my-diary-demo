fd3cb653fae3ec4d2751c4086586465aae08b92d
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-10-25 21:11:27 +0100

    Signup fix

--- src/components/containers/Signup.jsx
+++ src/components/containers/Signup.jsx
@@ -1,19 +1,64 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import SignupLayout from '../layouts/Signup';
 
 export default function () {
+  const [fname, setFname] = useState('');
+  const [email, setEmail] = useState('');
+  const [signupErr, setSignupErr] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [btnState, setBtnState] = useState(false);
+
   const history = useHistory();
 
   const handleSubmit = () => {
-    history.push('/home');
+    try {
+      if (!btnState) {
+        setBtnState(true);
+      }
+      const inputData = {
+        fullName: fname,
+        email,
+        username,
+        password,
+      };
+      const reqURL = 'http://localhost:5000/api/v1/auth/signup' || 'https://diary-app-demo.herokuapp.com/api/v1/auth/signup';
+      fetch(reqURL, {
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        method: 'POST',
+        body: JSON.stringify(inputData),
+      }).then((response) => response.json())
+        .then(({ error, token }) => {
+          if (error) {
+            if (error.messages) setSignupErr(error.messages[error.messages.length - 1].msg);
+            else if (error.message) setSignupErr(error.message);
+            setBtnState(false);
+          } else {
+            localStorage.setItem('token', token);
+            history.push('/home');
+          }
+        });
+    } catch (err) {
+      console.log(err);
+      setBtnState(false);
+    }
   };
 
   return (
     <>
       <SignupLayout
         handleSubmit={handleSubmit}
+        setFname={setFname}
+        setEmail={setEmail}
+        setUsername={setUsername}
+        setPassword={setPassword}
+        formBtnState={btnState}
+        signupErr={signupErr}
       />
     </>
   );
